f22852bddbd1b722ddc2b3f2c528640f4e3fddc0
Mariana Rinaldi <rinaldi.mari@gmail.com>
2022-09-29 00:23:25 -0300

    alterações nos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
 import { Post } from './Post'
+
+import './global.css';
 
 export function App() {
   return (
     <div>
-      <Post/>
+      <Header/>
+      <div className="wrapper">
+        
+      </div>
     </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css';
+import feedLogo from '../assets/feed-logo.svg';
+
+export function Header(){
+  return(
+    <header className={styles.header}>
+      <img src={feedLogo} alt="Logotipo do feed" />
+    </header>
+  );
+}
